5dad50dc93647fec9aef8bc7731f038019544800
brian presta <brian.presta@gmail.comgit commit -m first>
2020-10-07 18:39:14 -0700

    set up singlethought

---
+++ client/src/pages/index.js
@@ -0,0 +1,14 @@
+import Login from './Login'
+import NoMatch from './NoMatch'
+import SingleThought from './SingleThought'
+import Profile from './Profile'
+import Signup from './Signup'
+import Home from './Home'
+export default {
+    Login,
+    NoMatch,
+    SingleThought,
+    Profile,
+    Signup,
+    Home
+}
